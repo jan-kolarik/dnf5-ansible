2af5525da391f033563363927c4076d96f9adc15
Jan Kolarik <jkolarik@redhat.com>
2023-02-22 10:08:26 +0000

    Update script to match latest DNF5 nightly + add missing functionality examples

--- dnf5.py
+++ dnf5.py
@@ -3,17 +3,16 @@ import libdnf5
 
 # TODO DNF5 not-implemented features:
 # - some ensure modifiers
-#   - allow_downgrade
+#   - allow_downgrade - now it is always True
 # - autoremove
 #   - listing unneeded pkgs not implemented yet
+#   - https://github.com/rpm-software-management/dnf5/issues/132
 # - cache update
 #   - automatic in DNF5
 #   - any modification needed for the Ansible purposes?
-
-# TODO examples:
-# - configuration options like enable-repo
-# - limit upgrade packages only to bugfix / security ones
-# - update_only
+# - cacheonly
+#   - https://github.com/rpm-software-management/dnf5/issues/191
+# - disable_plugin, enable_plugin
 
 # DNF5 example usecases for the Ansible module
 #
@@ -33,37 +32,28 @@ class Dnf5AnsibleUsecases:
     def __init__(self):
         self.base = self._prepare_base()
         self._prepare_repos()
-    
+
     def _is_spec_installed(self, spec):
         settings = libdnf5.base.ResolveSpecSettings()
         query = libdnf5.rpm.PackageQuery(self.base)
         query.filter_installed()
         match, nevra = query.resolve_pkg_spec(spec, settings, True)
         return match
-    
-    # Note: Downloading part to be simplified
-    # See https://github.com/rpm-software-management/dnf5/issues/277
-    def _download_missing_packages(self, transaction):
-        downloader = libdnf5.repo.PackageDownloader()
 
-        for pkg in transaction.get_transaction_packages():
-            if libdnf5.base.transaction.transaction_item_action_is_inbound(pkg.get_action()) and \
-               pkg.get_package().get_repo().get_type() != libdnf5.repo.Repo.Type_COMMANDLINE:
-                downloader.add(pkg.get_package())
-
-        downloader.download(True, True)
-    
     def _do_transaction(self, transaction):
         # Add download / transaction callbacks, triggers
         # See 'dnf5/test/python3/libdnf5/tutorial/transaction/transaction.py'
-        
-        self._download_missing_packages(transaction)
-        
+
+        # Download all needed packages
+        # Optionally specify the 'download_dir' parameter
+        transaction.download()
+
         # Add other transaction metadata to be stored in the history DB
         # transaction.set_comment("Some comment")
         # transaction.set_user_id(12345)
-        
         transaction.set_description("Ansible")
+
+        # Execute the transaction
         transaction.run()
 
     def _package_dict(self, package):
@@ -77,25 +67,69 @@ class Dnf5AnsibleUsecases:
 
         return result
 
-    def _prepare_base(self):
-        base = libdnf5.base.Base()
-        base.load_config_from_file()
-        
-        # Override any configuration options like this:
-        # conf = base.get_config()
-        # conf.installroot().set('/path/to/installroot')
-        # conf.skip_broken().set(True)
-        #
+    def _override_base_conf(self, base):
         # Note: This is a subject of potential rewrite
         # See https://github.com/rpm-software-management/dnf5/issues/236
+
+        conf = base.get_config()
+        conf.best().set(True)
+        conf.disable_excludes().set([])
+        conf.excludepkgs().set([])
+        conf.gpgcheck().set(False)
+        conf.install_weak_deps().set(True)
+        conf.installroot().set('/root/dir/')
+        conf.repo_gpgcheck().set(False)
+        conf.skip_broken().set(True)
+        conf.sslverify().set(True)
+
+        vars = base.get_vars()
+        vars.set('releasever', '38')
+
+    def _prepare_base(self):
+        base = libdnf5.base.Base()
+
+        # Change config file path like this:
+        # base.config_file_path().set('path')
+
+        base.load_config_from_file()
+
+        # Override any configuration options here like this:
+        # self._override_base_conf(base)
 
         base.setup()
 
         return base
 
+    def _enable_repos(self, repos):
+        repo_query = libdnf5.repo.RepoQuery(self.base)
+        repo_query.filter_id(repos)
+        for repo in repo_query:
+            repo.enable()
+
+    def _disable_repos(self, repos):
+        repo_query = libdnf5.repo.RepoQuery(self.base)
+        repo_query.filter_id(repos)
+        for repo in repo_query:
+            repo.disable()
+
+    def _create_goal_job_settings(self):
+        settings = libdnf5.base.GoalJobSettings()
+
+        # Apply security options like this:
+        # advisory_query = libdnf5.advisory.AdvisoryQuery(self.base)
+        # advisory_query.filter_type('bugfix')
+        # settings.set_advisory_filter(advisory_query)
+
+        return settings
+
     def _prepare_repos(self):
         sack = self.base.get_repo_sack()
         sack.create_repos_from_system_configuration()
+
+        # Enable/disable repos here like this:
+        self._disable_repos(['updates'])
+        # self._enable_repos(list)
+
         sack.update_and_load_enabled_repos(True)
 
     def list(self, args):
@@ -128,7 +162,7 @@ class Dnf5AnsibleUsecases:
                 query.resolve_pkg_spec(spec, resolve_spec_settings, True)
                 results += [self._package_dict(package) for package in query]
             return results
-    
+
     def ensure(self, cmd, specs):
         """Install, update, remove usecase
 
@@ -137,17 +171,21 @@ class Dnf5AnsibleUsecases:
             specs (list[str]): Specs to be processed within the action
         """
         goal = libdnf5.base.Goal(self.base)
+        settings = self._create_goal_job_settings()
+
         if cmd == 'latest' and specs[0] == '*':
-            goal.add_rpm_upgrade()
+            goal.add_rpm_upgrade(settings)
         elif cmd in ['installed', 'present']:
             for spec in specs:
-                goal.add_install(spec)
+                goal.add_install(spec, settings)
         elif cmd == 'latest':
             for spec in specs:
-                goal.add_upgrade(spec)
+                # Filter only installed packages on update_only like this:
+                # if update_only and self._is_spec_installed(spec):
+                goal.add_upgrade(spec, settings)
         elif cmd == 'absent':
             for spec in specs:
-                goal.add_remove(spec)
+                goal.add_remove(spec, settings)
 
         # Apply behavior modifiers like this:
         # goal.set_allow_erasing(True)
